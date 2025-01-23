d7a14bb16d82e3c16a621eafac9a56fa07fdee7c
carolina-silva <carol_paiva@outlook.com>
2025-01-23 15:21:49 -0300

    :sparkles: Feat: Add script to convert XML to CSV

---
+++ src/utils/xml2dvc.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import argparse
+
+def xml_to_csv(input_path, output_path):
+    df= pd.read_xml(input_path)
+    df.to_csv(output_path, index=False)
+    
+    print(f"Arquivo {input_path} foi convertido e esta salvo na pasta {output_path}")
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Converter XML para CSV")
+    parser.add_argument("input", help="Caminha para o XML de entrada")
+    parser.add_argument("output", help="Caminho para salvar CSV")
+    args = parser.parse_args()
+    print(args.output)
+    
+    
+    xml_to_csv(args.input, args.output)
+    
